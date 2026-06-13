ac9fa96c0884724c2e5c85625da1ae8f82951ec2
PalkiainEarth-k23 <k2reddy23@gmail.com>
2026-06-14 00:09:43 +0530

    Add files via upload
    A Python program that uses Kepler's Third Law of Planetary Motion to calculate how many Earth days a year lasts on any exoplanet. The user inputs the planet's name, its star's name, the orbital distance (in AU), and the host star's mass. By leveraging the Python `math` module and standard arithmetic operators, the script determines the precise orbital period.

---
+++ Length_of_Years_In_Alien_Worlds.py
@@ -0,0 +1,8 @@
+import math 
+Planet= input("what is the name of the planet:")
+host_star = input("What is the name of the host star: ")
+Distance = float(input(f"What the distance of {Planet} from {host_star} in AU: "))
+Mass = float(input(f"What is the mass of the {host_star}: "))
+Orbital_Time_in_Earth_yrs= math.sqrt(Distance * Distance*Distance/ Mass)
+Orbital_Time_in_Days = Orbital_Time_in_Earth_yrs*365.25
+print(f"Thus the Orbital time of {Planet} around the {host_star} is {Orbital_Time_in_Earth_yrs} Earth years or {Orbital_Time_in_Days} Earth Days")
